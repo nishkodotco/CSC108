66f7cd3baa825991afd9b7700bc538577c976adc
nishko <nishkodotco@gmail.com>
2026-09-11 17:59:21 +0000

    updated the container value.

--- .devcontainer/features/python/install.sh
+++ .devcontainer/features/python/install.sh
@@ -3,5 +3,13 @@ set -euxo pipefail
 
 # runs as root during build
 microdnf update -y
-microdnf install -y python3 python3-pip
+
+microdnf install -y \
+    python3 \
+    python3-pip
+
 microdnf clean all
+
+# Verify installation
+python3 --version
+python3 -m pip --version
