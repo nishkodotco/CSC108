f113dd584dbde306c8b5c9e7dcaf22bc6be3061e
nishko <nishkodotco@gmail.com>
2026-09-22 17:52:12 +0000

    added the week 3 assigment.

---
+++ week_3.py
@@ -0,0 +1,23 @@
+# you have to input three number ---> sides of the triangle.
+
+def is_right_triangle(a:float,b:float,c:float):
+    """
+    is_right_triangle(3,4,5)
+    >>> true
+
+    is_trigh_triangle(6,6,6)
+    >>> false
+
+    print("true")
+
+
+    statement 1 ---> 0
+    statement 2 ---> 24
+    statement 3 ---> -20
+
+
+    """
+    print("true")
+    return True
+
+is_right_triangle(3,4,5)
